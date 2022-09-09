3a4e5ff36b8eab33e2d29c74975ac92528b78c78
Emrah Yurtlu <emrahyurtlu@gmail.com>
2022-09-09 19:00:23 +0300

    Project is initialized

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ExpenseItem from "./components/ExpenseItem";
 
-function App() {
+const App = () => {
+  const expenses = [
+    {title: 'Car Insurance', amount: 294.21, date: new Date(2020, 7, 1)},
+    {title: 'Toilet', amount: 94.21, date: new Date(2021, 8, 15)},
+    {title: 'Meat', amount: 194.21, date: new Date(2022, 10, 29)},
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello World!!!</h1>
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
     </div>
   );
-}
+};
 
 export default App;
