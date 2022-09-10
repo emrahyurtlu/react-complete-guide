9339148378be753cffdc7e55b6cca1dd4ac9273b
Emrah Yurtlu <emrahyurtlu@gmail.com>
2022-09-10 23:06:17 +0300

    expense item is decomposed.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import "./App.css";
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses"
 
 const App = () => {
   const expenses = [
-    {title: 'Car Insurance', amount: 294.21, date: new Date(2020, 7, 1)},
-    {title: 'Toilet', amount: 94.21, date: new Date(2021, 8, 15)},
-    {title: 'Meat', amount: 194.21, date: new Date(2022, 10, 29)},
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
   ];
 
   return (
     <div>
-      <h1>Hello World!!!</h1>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 };
